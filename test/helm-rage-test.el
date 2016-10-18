85f92390e4ba454eafc153092025200143344a62
Patrick Haun <bomgar85@googlemail.com>
2016-10-18 15:03:54 +0200

    add you don't say meme

--- test/helm-rage-test.el
+++ test/helm-rage-test.el
@@ -1,6 +1,3 @@
 
-(ert-deftest helm-rage-test-get-memes ()
-  (should (equal 1 (length (helm-rage-get-memes)))))
-
 (ert-deftest helm-rage-test-meme-alist ()
   (should (equal (cons "bitch please" "bitch_please.meme") (assoc "bitch please" (helm-rage-meme-alist)))))
