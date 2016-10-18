1856f54c6f905fd78dd47a1d91f9bc504b20ed65
Patrick Haun <bomgar85@googlemail.com>
2016-10-18 14:33:21 +0200

    cleanup

--- helm-rage.el
+++ helm-rage.el
@@ -1,22 +1,11 @@
-;;; helm-rage.el --- Helm command for rage characters. -*- lexical-binding: t -*-
+;;; helm-rage.el --- Helm command for rage characters.
 
 ;; Copyright © 2016 Patrick Haun
 
 ;; Version: 0.0.1
 ;; Package-Requires: ((helm "1.9.8") (emacs "24.4") (dash "2.13.0"))
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; Keywords: helm, rage, meme
+;; URL: https://github.com/bomgar/helm-rage
 
 ;;; Commentary:
 ;; A helm command for raging.
@@ -38,8 +27,7 @@
 
 (defun helm-rage-meme-alist ()
   "Gets the list of all memes with their name."
-  (-map (lambda (meme-file) (cons (replace-regexp-in-string "\.meme" "" (replace-regexp-in-string "_" " " meme-file)) meme-file)) (helm-rage-get-memes))
-  )
+  (-map (lambda (meme-file) (cons (replace-regexp-in-string "\.meme" "" (replace-regexp-in-string "_" " " meme-file)) meme-file)) (helm-rage-get-memes)))
 
 (defun helm-rage-source ()
   "Builds the helm rage source."
